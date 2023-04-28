8c61ac1efae4b27a5d56b58d580dd568385795de
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-04-28 16:05:06 -0700

    add migrations

---
+++ Karmaplusplus/Migrations/20230428230338_AddModels.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Karmaplusplus.Migrations
+{
+    public partial class AddModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ServiceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ServiceName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ZipCode = table.Column<float>(type: "float", nullable: false),
+                    UserId = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ServiceId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "ServiceId", "Description", "Email", "ServiceName", "UserId", "ZipCode" },
+                values: new object[] { 1, "Free English lessons", "english.lessons@gmail.com", "lesson", null, 98052f });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "ServiceId", "Description", "Email", "ServiceName", "UserId", "ZipCode" },
+                values: new object[] { 2, "Free haircuts for men, women and children", "free.haircuts@gmail.com", "haircut", null, 98040f });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "ServiceId", "Description", "Email", "ServiceName", "UserId", "ZipCode" },
+                values: new object[] { 3, "Free babysitting for single mothers at your home", "free.babysitting@gmail.com", "babysitting", null, 98033f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Services");
+        }
+    }
+}
--- Karmaplusplus/Models/Service.cs
+++ Karmaplusplus/Models/Service.cs
@@ -3,7 +3,7 @@ namespace Karmaplusplus.Models
   public class Service
   {
     public int ServiceId { get; set; }
-    public string Service { get; set; }
+    public string ServiceName { get; set; }
     public string Description { get; set; }
     public string Email { get; set; }
     public float ZipCode { get; set; }
